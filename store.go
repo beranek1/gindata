74a4fbbce6b2268624d442915b6bb5abcb06aed6
Thomas Beranek <github@beranek.one>
2023-01-07 12:34:05 +0000

    Initialize module.

---
+++ store.go
@@ -0,0 +1,161 @@
+package gindata
+
+import (
+	"encoding/json"
+	"strconv"
+
+	"github.com/beranek1/godatainterface"
+	"github.com/gin-gonic/gin"
+)
+
+type DataStoreErrorResponse struct {
+	Error any
+}
+
+type DataStoreGetResponse struct {
+	Data any
+}
+
+type DataStoreBackend struct {
+	store godatainterface.DataStoreVersionedRangeFromInterval
+}
+
+func CreateDataStoreBackend(store godatainterface.DataStoreVersionedRangeFromInterval) *DataStoreBackend {
+	return &DataStoreBackend{store: store}
+}
+
+func returnAsJsonResponse(c *gin.Context, data any, code int) {
+	byt, err := json.Marshal(data)
+	if err != nil {
+		byt, err := json.Marshal(DataStoreGetResponse{err.Error()})
+		if err != nil {
+			c.String(500, "{\"Error\":\""+err.Error()+"\"}")
+		} else {
+			c.String(500, string(byt))
+		}
+	} else {
+		c.String(code, string(byt))
+	}
+}
+
+func (b *DataStoreBackend) Get(c *gin.Context) {
+	key := c.Param("key")
+	value, err := b.store.Get(key)
+	if err != nil {
+		returnAsJsonResponse(c, DataStoreErrorResponse{err.Error()}, 404)
+		return
+	}
+	returnAsJsonResponse(c, DataStoreGetResponse{value}, 200)
+}
+
+func (b *DataStoreBackend) GetAt(c *gin.Context) {
+	key := c.Param("key")
+	timestamp, err := strconv.ParseInt(c.Param("timestamp"), 10, 64)
+	if err != nil {
+		returnAsJsonResponse(c, DataStoreErrorResponse{err.Error()}, 400)
+		return
+	}
+	value, err := b.store.GetAt(key, timestamp)
+	if err != nil {
+		returnAsJsonResponse(c, DataStoreErrorResponse{err.Error()}, 404)
+		return
+	}
+	returnAsJsonResponse(c, DataStoreGetResponse{value}, 200)
+}
+
+func (b *DataStoreBackend) Range(c *gin.Context) {
+	key := c.Param("key")
+	start, err := strconv.ParseInt(c.Param("start"), 10, 64)
+	if err != nil {
+		returnAsJsonResponse(c, DataStoreErrorResponse{err.Error()}, 400)
+		return
+	}
+	end, err := strconv.ParseInt(c.Param("end"), 10, 64)
+	if err != nil {
+		returnAsJsonResponse(c, DataStoreErrorResponse{err.Error()}, 400)
+		return
+	}
+	values, err := b.store.Range(key, start, end)
+	if err != nil {
+		returnAsJsonResponse(c, DataStoreErrorResponse{err.Error()}, 404)
+		return
+	}
+	returnAsJsonResponse(c, DataStoreGetResponse{values}, 200)
+}
+
+func (b *DataStoreBackend) From(c *gin.Context) {
+	key := c.Param("key")
+	start, err := strconv.ParseInt(c.Param("start"), 10, 64)
+	if err != nil {
+		returnAsJsonResponse(c, DataStoreErrorResponse{err.Error()}, 400)
+		return
+	}
+	values, err := b.store.From(key, start)
+	if err != nil {
+		returnAsJsonResponse(c, DataStoreErrorResponse{err.Error()}, 404)
+		return
+	}
+	returnAsJsonResponse(c, DataStoreGetResponse{values}, 200)
+}
+
+func (b *DataStoreBackend) RangeInterval(c *gin.Context) {
+	key := c.Param("key")
+	start, err := strconv.ParseInt(c.Param("start"), 10, 64)
+	if err != nil {
+		returnAsJsonResponse(c, DataStoreErrorResponse{err.Error()}, 400)
+		return
+	}
+	end, err := strconv.ParseInt(c.Param("end"), 10, 64)
+	if err != nil {
+		returnAsJsonResponse(c, DataStoreErrorResponse{err.Error()}, 400)
+		return
+	}
+	interval, err := strconv.ParseInt(c.Param("interval"), 10, 64)
+	if err != nil {
+		returnAsJsonResponse(c, DataStoreErrorResponse{err.Error()}, 400)
+		return
+	}
+	values, err := b.store.RangeInterval(key, start, end, interval)
+	if err != nil {
+		returnAsJsonResponse(c, DataStoreErrorResponse{err.Error()}, 404)
+		return
+	}
+	returnAsJsonResponse(c, DataStoreGetResponse{values}, 200)
+}
+
+func (b *DataStoreBackend) FromInterval(c *gin.Context) {
+	key := c.Param("key")
+	start, err := strconv.ParseInt(c.Param("start"), 10, 64)
+	if err != nil {
+		returnAsJsonResponse(c, DataStoreErrorResponse{err.Error()}, 400)
+		return
+	}
+	interval, err := strconv.ParseInt(c.Param("interval"), 10, 64)
+	if err != nil {
+		returnAsJsonResponse(c, DataStoreErrorResponse{err.Error()}, 400)
+		return
+	}
+	values, err := b.store.FromInterval(key, start, interval)
+	if err != nil {
+		returnAsJsonResponse(c, DataStoreErrorResponse{err.Error()}, 404)
+		return
+	}
+	returnAsJsonResponse(c, DataStoreGetResponse{values}, 200)
+}
+
+func (b *DataStoreBackend) AttachToRouter(path string, r *gin.Engine) {
+	r.GET(path+"/:key", b.Get)
+	r.GET(path+"/:key/at/:timestamp", b.GetAt)
+	r.GET(path+"/:key/range/:start/:end/:interval", b.RangeInterval)
+	r.GET(path+"/:key/range/:start/:end", b.Range)
+	r.GET(path+"/:key/range/:start", b.From)
+	r.GET(path+"/:key/from/:start", b.From)
+	r.GET(path+"/:key/from/:start/:interval", b.FromInterval)
+}
+
+func (b *DataStoreBackend) SetupRouter() *gin.Engine {
+	r := gin.Default()
+	r.SetTrustedProxies(nil)
+	b.AttachToRouter("", r)
+	return r
+}
---
+++ store_test.go
@@ -0,0 +1,160 @@
+package gindata
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
+)
+
+type dataStoreMock struct {
+	success      bool
+	Last_call    string
+	Return_value any
+	Return_map   map[int64]any
+}
+
+func (m *dataStoreMock) Get(key string) (any, error) {
+	m.Last_call = "Get " + key
+	if m.success {
+		return m.Return_value, nil
+	}
+	return nil, errors.New("so sorry")
+}
+
+func (m *dataStoreMock) GetAt(key string, timestamp int64) (any, error) {
+	m.Last_call = "GetAt " + key + " " + fmt.Sprint(timestamp)
+	if m.success {
+		return m.Return_value, nil
+	}
+	return nil, errors.New("so sorry")
+}
+
+func (m *dataStoreMock) Put(key string, value any) error {
+	m.Last_call = "Put " + key + " " + fmt.Sprint(value)
+	if m.success {
+		return nil
+	}
+	return errors.New("so sorry")
+}
+
+func (m *dataStoreMock) PutAt(key string, value any, timestamp int64) error {
+	m.Last_call = "PutAt " + key + " " + fmt.Sprint(value) + " " + fmt.Sprint(timestamp)
+	if m.success {
+		return nil
+	}
+	return errors.New("so sorry")
+}
+
+func (m *dataStoreMock) Range(key string, start int64, end int64) (map[int64]any, error) {
+	m.Last_call = "Range " + key + " " + fmt.Sprint(start) + " " + fmt.Sprint(end)
+	if m.success {
+		return m.Return_map, nil
+	}
+	return nil, errors.New("so sorry")
+}
+
+func (m *dataStoreMock) From(key string, start int64) (map[int64]any, error) {
+	m.Last_call = "From " + key + " " + fmt.Sprint(start)
+	if m.success {
+		return m.Return_map, nil
+	}
+	return nil, errors.New("so sorry")
+}
+
+func (m *dataStoreMock) RangeInterval(key string, start int64, end int64, interval int64) (map[int64]any, error) {
+	m.Last_call = "RangeInterval " + key + " " + fmt.Sprint(start) + " " + fmt.Sprint(end) + " " + fmt.Sprint(interval)
+	if m.success {
+		return m.Return_map, nil
+	}
+	return nil, errors.New("so sorry")
+}
+
+func (m *dataStoreMock) FromInterval(key string, start int64, interval int64) (map[int64]any, error) {
+	m.Last_call = "FromInterval " + key + " " + fmt.Sprint(start) + " " + fmt.Sprint(interval)
+	if m.success {
+		return m.Return_map, nil
+	}
+	return nil, errors.New("so sorry")
+}
+
+func createTestBackendRouterSuccess() (*gin.Engine, *dataStoreMock) {
+	rv := "value"
+	rm := map[int64]any{}
+	rm[123] = rv
+	m := &dataStoreMock{true, "", rv, rm}
+	b := CreateDataStoreBackend(m)
+	return b.SetupRouter(), m
+}
+
+func createTestBackendRouterError() (*gin.Engine, *dataStoreMock) {
+	m := &dataStoreMock{false, "", nil, nil}
+	b := CreateDataStoreBackend(m)
+	return b.SetupRouter(), m
+}
+
+func testPath(t *testing.T, method string, path string, code int, response string, call string) {
+	router, m := createTestBackendRouterSuccess()
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest(method, path, nil)
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, code, w.Code)
+	assert.Equal(t, response, w.Body.String())
+	assert.Equal(t, m.Last_call, call)
+
+	erouter, _ := createTestBackendRouterError()
+
+	ew := httptest.NewRecorder()
+	ereq, _ := http.NewRequest(method, path, nil)
+	erouter.ServeHTTP(ew, ereq)
+
+	assert.Equal(t, 404, ew.Code)
+	assert.Equal(t, "{\"Error\":\"so sorry\"}", ew.Body.String())
+}
+
+func testIllegalPath(t *testing.T, method string, path string, code int, response string) {
+	router, m := createTestBackendRouterSuccess()
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest(method, path, nil)
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, code, w.Code)
+	assert.Equal(t, response, w.Body.String())
+	assert.Equal(t, m.Last_call, "")
+}
+
+func TestGet(t *testing.T) {
+	testPath(t, "GET", "/test", 200, "{\"Data\":\"value\"}", "Get test")
+}
+
+func TestGetAt(t *testing.T) {
+	testPath(t, "GET", "/test/at/456", 200, "{\"Data\":\"value\"}", "GetAt test 456")
+	testIllegalPath(t, "GET", "/test/at/abc", 400, "{\"Error\":\"strconv.ParseInt: parsing \\\"abc\\\": invalid syntax\"}")
+}
+
+func TestRange(t *testing.T) {
+	testPath(t, "GET", "/test/range/456", 200, "{\"Data\":{\"123\":\"value\"}}", "From test 456")
+	testPath(t, "GET", "/test/range/456/789", 200, "{\"Data\":{\"123\":\"value\"}}", "Range test 456 789")
+	testPath(t, "GET", "/test/range/456/789/10", 200, "{\"Data\":{\"123\":\"value\"}}", "RangeInterval test 456 789 10")
+	testIllegalPath(t, "GET", "/test/range/abc", 400, "{\"Error\":\"strconv.ParseInt: parsing \\\"abc\\\": invalid syntax\"}")
+	testIllegalPath(t, "GET", "/test/range/abc/789", 400, "{\"Error\":\"strconv.ParseInt: parsing \\\"abc\\\": invalid syntax\"}")
+	testIllegalPath(t, "GET", "/test/range/456/def", 400, "{\"Error\":\"strconv.ParseInt: parsing \\\"def\\\": invalid syntax\"}")
+	testIllegalPath(t, "GET", "/test/range/abc/789/10", 400, "{\"Error\":\"strconv.ParseInt: parsing \\\"abc\\\": invalid syntax\"}")
+	testIllegalPath(t, "GET", "/test/range/456/def/10", 400, "{\"Error\":\"strconv.ParseInt: parsing \\\"def\\\": invalid syntax\"}")
+	testIllegalPath(t, "GET", "/test/range/456/789/ghi", 400, "{\"Error\":\"strconv.ParseInt: parsing \\\"ghi\\\": invalid syntax\"}")
+}
+
+func TestFrom(t *testing.T) {
+	testPath(t, "GET", "/test/from/456", 200, "{\"Data\":{\"123\":\"value\"}}", "From test 456")
+	testPath(t, "GET", "/test/from/456/10", 200, "{\"Data\":{\"123\":\"value\"}}", "FromInterval test 456 10")
+	testIllegalPath(t, "GET", "/test/from/abc", 400, "{\"Error\":\"strconv.ParseInt: parsing \\\"abc\\\": invalid syntax\"}")
+	testIllegalPath(t, "GET", "/test/from/abc/789", 400, "{\"Error\":\"strconv.ParseInt: parsing \\\"abc\\\": invalid syntax\"}")
+	testIllegalPath(t, "GET", "/test/from/456/def", 400, "{\"Error\":\"strconv.ParseInt: parsing \\\"def\\\": invalid syntax\"}")
+}
